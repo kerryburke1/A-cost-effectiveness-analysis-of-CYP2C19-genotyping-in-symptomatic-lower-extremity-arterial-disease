0f3bce623ea9a63f150eb7bf1087d4497f8959c8
kerryburke1 <kerry.burke@nhs.net>
2026-08-04 14:19:55 +0100

    Update CLTI - clopi.R

--- Deterministic model/CLTI - clopi.R
+++ Deterministic model/CLTI - clopi.R
@@ -1,5 +1,5 @@
 ncycles<-54
-n<-markovn2[4]
+n<-markovn2[2]
 
 #Transition Matrix            
 transmat<-matrix(c(probAtoA_6monthC,probAtoB_6monthC,probAtoC_6monthC,0,probAtoE_6monthC,0,0,probAtoAH_6monthC,0,0,0,0,0,0,probAtoAI_6monthC,0,0,0,0,0,0,probAtoJ_6monthC,p_death,
