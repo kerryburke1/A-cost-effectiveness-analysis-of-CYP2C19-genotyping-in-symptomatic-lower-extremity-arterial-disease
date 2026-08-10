339f6960c08e8c5d89db815390a9ad6307ca08e2
kerryburke1 <kerry.burke@nhs.net>
2026-08-10 16:43:43 +0100

    Update CLTI - clopi no test1.R

--- Deterministic model/CLTI - clopi no test1.R
+++ Deterministic model/CLTI - clopi no test1.R
@@ -183,7 +183,7 @@ markovtrace["disccost"] <- markovtrace["Cost"] * markovtrace["disc"]
 markovtrace["discqaly"] <- markovtrace["QALY"] * markovtrace["disc"]
 
 results <- c(
-  sum(head(markovtrace$disccost, ncycles)) + (n * costTest) + sum(tot_trans_cost),
+  sum(head(markovtrace$disccost, ncycles)) + sum(tot_trans_cost),
   sum(head(markovtrace$discqaly, ncycles))
 )
 CLTInotest1<-results
