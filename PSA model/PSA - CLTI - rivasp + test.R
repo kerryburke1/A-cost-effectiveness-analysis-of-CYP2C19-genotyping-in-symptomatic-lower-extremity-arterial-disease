6456fc3a68ebde48631f22e27f3abc3649873295
kerryburke1 <kerry.burke@nhs.net>
2026-08-10 17:00:18 +0100

    Update PSA - CLTI - rivasp + test.R

--- PSA model/PSA - CLTI - rivasp + test.R
+++ PSA model/PSA - CLTI - rivasp + test.R
@@ -215,12 +215,9 @@ for(j in 1:mcruns){
   tot_trans_cost <- numeric(ncycles)
   
   for(i in 1:ncycles){
-    df <- 1/(1 + discountrate)^(i/2)
-    
-    tot_trans_cost[i] <-
-      sum(translist[[i]] * trans_cost_mat) * df
-  }
-  
+  df <- 1/(1 + discountrate)^((i-1)/2)   # beginning of cycle i
+  tot_trans_cost[i] <- sum(translist[[i]] * trans_cost_mat) * df
+}
   sum(tot_trans_cost)
   
   markovtrace["disc"] <- 1 / ((1 + discountrate)^(markovtrace["SixMonths"] * 0.5))
