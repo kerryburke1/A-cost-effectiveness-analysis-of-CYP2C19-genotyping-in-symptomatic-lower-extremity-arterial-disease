bfb32bf702c8c9fdd852f10235c24afcc5c8e72b
kerryburke1 <kerry.burke@nhs.net>
2026-08-11 14:30:28 +0100

    Update PSA - CLTI - asp + test.R

--- PSA model/PSA - CLTI - asp + test.R
+++ PSA model/PSA - CLTI - asp + test.R
@@ -157,13 +157,11 @@ for(j in 1:mcruns){
     (markovtrace["VascularDeath"]*qolJ)+
     (markovtrace["OtherDeath"]*qolK)
   
-  tot_trans_cost <- numeric(ncycles)
-  
- for(i in 1:ncycles){
-  df <- 1/(1 + discountrate)^((i-1)/2)   # beginning of cycle i
-  tot_trans_cost[i] <- sum(translist[[i]] * trans_cost_mat) * df
-}
-  sum(tot_trans_cost)
+tot_trans_cost <- numeric(ncycles) 
+for(i in 1:ncycles){ 
+df <- 1 / ((1 + discountrate)^((i - 0.5) * 0.5)) 
+tot_trans_cost[i] <- sum(translist[[i]] * trans_cost_mat) * df }
+sum(tot_trans_cost)
   
 hcc_cost <- numeric(ncycles)
 hcc_qaly <- numeric(ncycles)
